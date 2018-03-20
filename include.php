451ff2b41202515e6f192fd3df6c1414c914d084
James P. Galovic <jpgalovic@gmail.com>
2018-03-21 01:11:08 +1100

    Add: Code_Gen Includes File.

---
+++ include.php
@@ -0,0 +1,10 @@
+<?php
+	/**
+	 * SwinAnime x STARS, Code_Gen Includes, Used to Include all Default Code_Gen files
+	 * @version v0.1.1
+	 * @author J.P. Galovic
+	 * @date MAR18
+	 */
+
+	include('code_gen/time_keeping.php');
+?>
