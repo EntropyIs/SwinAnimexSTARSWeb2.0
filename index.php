1dda938f5dd947e860f32f032d38e4d076a5f507
James P. Galovic <jpgalovic@gmail.com>
2018-03-21 01:23:29 +1100

    Add: Index

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+	/**
+	 * SwinAnime x STARS, Index Page, Used to build all publically accessable pages on club's website.
+	 * @version v0.1.1
+	 * @author J.P. Galovic
+	 * @date MAR18
+	 */
+
+	require_once('include.php'); // Include rquired files.
+
+	//Test Time Code! 21/03/2018
+	echo(TimeKeeping::now());
+?>
