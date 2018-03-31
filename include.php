465d8b205870a637cec2681fc86f382821c9b163
James P. Galovic <jpgalovic@gmail.com>
2018-04-01 02:02:38 +1100

    Add: SQL_Core to Include's

--- include.php
+++ include.php
@@ -1,11 +1,12 @@
 <?php
 	/**
 	 * SwinAnime x STARS, Code_Gen Includes, Used to Include all Default Code_Gen files
-	 * @version v0.1.1
+	 * @version v0.1.3
 	 * @author J.P. Galovic
-	 * @date MAR18
+	 * @date APR18
 	 */
 
 	require_once('code_gen/time_keeping.php');
 	require_once('code_gen/process.php');
+	require_once('code_gen/sql_core.php');
 ?>
