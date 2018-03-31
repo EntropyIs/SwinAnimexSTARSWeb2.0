ad73f9250e51d204a66f42710c22fd0188f0b98b
James P. Galovic <jpgalovic@gmail.com>
2018-04-01 01:44:41 +1100

    Add: Illegal Char Removal Function

--- code_gen/process.php
+++ code_gen/process.php
@@ -1,9 +1,9 @@
 <?php
 	/**
 	 * SwinAnime x STARS, Process Class, Used for processing purposes
-	 * @version v0.1.1
+	 * @version v0.1.2
 	 * @author J.P. Galovic
-	 * @date MAR18
+	 * @date APR18
 	 */
 	define('URL_VERSION', '0.1.1');
 
@@ -17,6 +17,23 @@
 		 */
 		public static function auto_version($url) {
 			return($url.'?v='.URL_VERSION);
+		}
+		
+		/**
+		 * Remove Illegal Characters, removes illegal characters from a string
+		 * @param $string, string to remove characters from
+		 * @return $string with removed characters
+		 * @author J.P.Galovic
+		 * @date 01/04/2018
+		 */
+		public static function remove_illegal_char($string)
+		{
+			$illegal = array(
+				'~', '#', '%', '*', '{', '}', ':', '<', '>', '?', '/', '+', '|', '"', '.', ',', '\''
+			);
+			
+			$string = str_replace($illegal, '', $string);
+			return($string);
 		}
 	}
 ?>
